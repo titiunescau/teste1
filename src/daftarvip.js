f5bba48f383c25d42530b09f15115e710c612eb1
titiunescau <81128483+titiunescau@users.noreply.github.com>
2021-03-25 14:12:04 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/559887053394 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO NESCAU 🐊🚩 :*
+_https://chat.whatsapp.com/HDge9187a9G9hSDmURTaF3
+}
+exports.daftarvip = daftarvip
